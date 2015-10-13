b778d46685e7f6e7ce3ff6f7c6e8378ec70be002
Tina Wisneski <daydreamer525@rocketmail.com>
2015-10-13 15:24:09 -0500

    initial commit

---
+++ musichistoryversion2/musichistory2.js
@@ -0,0 +1,31 @@
+var songs = [];
+
+songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
+songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
+songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
+songs[songs.length] = "Welco(me to the Jungle > by Guns and Roses on the album Appetite for Destruction";
+songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
+
+
+for (var i = 0; i < scores.length; i++) {
+  songs = songs[i];
+  if(songs > highestScore) {
+  highestScore = songs;
+  }
+  if (songs < lowestScore){
+    lowestScore=songs;
+  } 
+}
+
+
+
+
+
+//  for (var i = 0; i <= 4; i++) {
+//  if (songs === songs [i]) {
+// ￼} 
+//  else
+// }
+
+
+
