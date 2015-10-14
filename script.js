fff84e2ef0f93fa590d78dd2bcdcf7ed01872854
Tina Wisneski <daydreamer525@rocketmail.com>
2015-10-14 13:21:15 -0500

    correct files

--- script.js
+++ script.js
@@ -1,17 +1,67 @@
+var songs = [];
+
+songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
+songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
+songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
+songs[songs.length] = "Welco(me to the Jungle > by Guns and Roses on the album Appetite for Destruction";
+songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
+
+console.log('the songs length is '+songs.length);
+
+songs.unshift("Down with the Sickness > Disturbed on the album Sickness");
+
+songs[songs.length] = "End Song Name";
+
+// songs[i] = songs
+    // .replace("*","")
+    // .replace("@","")
+    // .replace("(","")
+    // .replace("!","")
 
 
-var sonnetElement = document.getElementById("sonnet");
-var sonnetText = sonnetElement.innerHTML
-console.log("sonnetText", sonnetText);
+// songs[1]= "REMOVE LOGICAL SONG"
+// console.log("song at the 1 place")
+// console.log(songs[1]);
+// console.log("Additional Song = "+ songs[6]);
 
-var indexOfOrphans = sonnetText.indexOf("orphans");
-console.log("indexOfOrphans", indexOfOrphans);
-console.log("length of sonnet", sonnetText.length);
+// 1. JAMES CODE
+var destination = document.getElementById('injectionSpot')
+var masterString = "";
 
-sonnetText = sonnetText.replace("winter", "yuletide");
+var songElement
+for (var i = 0; i < songs.length; i++) {
+    var song = songs[i];
+    songs[i] = song
+    .replace(">", "-")
+    .replace("*","")
+    .replace("@","")
+    .replace("(","")
+    .replace("!","")
 
-sonnetElement.innerHTML = sonnetText;
+    console.log(song)
+    // 2. JAMES CODE
+    songElement = "<li ><span id='songname1' class='song-name'>" + songs[i] + "</span>"
+    masterString = masterString + songElement
+}
+console.log("final:")
+console.log( songs ); 
 
-sonnetElement.innerHTML = sonnetText.replace(/the/g, "a large");
+// 3. JAMES CODE 
+destination.innerHTML = masterString;
+console.log(masterString)
+
+
+"<li ><span id='songname1' class='song-name'>Legs - by Z*Ztop on the album Eliminator</span>"
+
+
+
+
+
+
+//  for (var i = 0; i <= 4; i++) {
+//  if (songs === songs [i]) {
+// ￼} 
+//  else
+// }
 
 
