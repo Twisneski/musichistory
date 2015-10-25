c152f5987db31807995a3115091340cae1d47a1b
Tina Wisneski <daydreamer525@rocketmail.com>
2015-10-25 17:25:35 -0500

    version 4

--- script.js
+++ script.js
@@ -1,68 +1,112 @@
-var songs = [];
+$(document).ready(function(){
 
-songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
-songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
-songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
-songs[songs.length] = "Welco(me to the Jungle > by Guns and Roses on the album Appetite for Destruction";
-songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
+    var entryMusic = $("#entryMusic");
+    entryMusic.hide();
 
-console.log('the songs length is '+songs.length);
+    var songs = [];
 
-songs.unshift("Down with the Sickness > Disturbed on the album Sickness");
+    songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
+    songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
+    songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
+    songs[songs.length] = "Welco(me to the Jungle > by Guns and Roses on the album Appetite for Destruction";
+    songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
 
-songs[songs.length] = "End Song Name";
+    console.log('the songs length is '+songs.length);
 
-// songs[i] = songs
-    // .replace("*","")
-    // .replace("@","")
-    // .replace("(","")
-    // .replace("!","")
+    songs.unshift("Down with the Sickness > Disturbed on the album Sickness");
 
+    songs[songs.length] = "End Song Name";
 
-// songs[1]= "REMOVE LOGICAL SONG"
-// console.log("song at the 1 place")
-// console.log(songs[1]);
-// console.log("Additional Song = "+ songs[6]);
+   
+    var destination = $('#injectionSpot')
+    var masterString = "";
 
-// 1. JAMES CODE
-var destination = document.getElementById('injectionSpot')
-var masterString = "";
+    var songElement
+    for (var i = 0; i < songs.length; i++) {
+        var song = songs[i];
+        songs[i] = song
+        .replace(">", "-")
+        .replace("*","")
+        .replace("@","")
+        .replace("(","")
+        .replace("!","")
+        console.log(song)
+       
+        songElement = "<li><span id='songname1' class='song-name'>"
+                         + songs[i] + "</span></li>"
+        masterString = masterString + songElement
+    }
+        console.log("final:")
+        console.log( songs ); 
 
-var songElement
-for (var i = 0; i < songs.length; i++) {
-    var song = songs[i];
-    songs[i] = song
-    .replace(">", "-")
-    .replace("*","")
-    .replace("@","")
-    .replace("(","")
-    .replace("!","")
-
-    console.log(song)
-    // 2. JAMES CODE
-    songElement = "<li >
-                    <span id='songname1' class='song-name'>"
-                     + songs[i] + "</span></li>"
-    masterString = masterString + songElement
-}
-console.log("final:")
-console.log( songs ); 
-
-// 3. JAMES CODE 
-destination.innerHTML = masterString;
-console.log(masterString)
+   
+    destination.html(masterString);
+    console.log(masterString)
 
 
-"<li ><span id='songname1' class='song-name'>Legs - by Z*Ztop on the album Eliminator</span>"
+    "<li ><span id='songname1' class='song-name'>Legs - by Z*Ztop on the album Eliminator</span>"
+
+    $("#addLink").click(unhide);
+
+    
+    function unhide (event) {
+        event.preventDefault();  
+        entryMusic.show();
+        $("#leftColumn").hide();
+        $("#rightColumn").hide();
+    
+    }
+
+    $("#viewMusic").click(hide);
+
+    function hide (event) {
+        event.preventDefault();
+        $("#rightColumn").show();
+        $("#leftColumn").show();
+        entryMusic.hide();
+
+    }
 
 
+    $("#addMusic").click();
+
+    function addToSongList (event) {
+        $("#enterMusic").val();
+        $("#userArtistInput").val();
+        $("#userAlbum").val();
+    
+    }
+
+    $("#Boxed").css ({
+        "border": "1px solid black",
+        "width": "1075px",
+        "height": "45px",
+        "background-color": "yellow",
+        "margin": "0 0 0 50px",
+        "padding": "15px 0 0 15px",
+        "border": "2px solid black",
+        "position": "fixed; top: 0em",
+
+});
 
 
+    document.getElementById("addMusic").onclick = function() {
+    var text = document.getElementById("userSongInput").value; 
+    var viewMusic = "<li>" + text + "</li>";
+    $("#injectionSpot").append(viewMusic);
+}  
+    document.getElementById("addMusic").onclick = function() {
+    var text = document.getElementById("userArtistInput").value; 
+    var viewMusic = "<li>" + text + "</li>";
+    $("#injectionSpot").append(viewMusic);
+}  
+    document.getElementById("addMusic").onclick = function() {
+    var text = document.getElementById("userAlbum").value; 
+    var viewMusic = "<li>" + text + "</li>";
+    $("#injectionSpot").append(viewMusic);
+}      
 
-//  for (var i = 0; i <= 4; i++) {
-//  if (songs === songs [i]) {
-// ￼} 
-//  else
-// }
+ 
 
 
+});
