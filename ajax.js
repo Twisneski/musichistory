05bb951cbf637194396f02ca097e0292e6dc7f3d
Tina Wisneski <daydreamer525@rocketmail.com>
2015-10-25 23:29:09 -0500

    final musichistory 5

--- ajax.js
+++ ajax.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
   console.log("documentReady");
 
-var masterString = "";
-
+  var masterString = "";
+  var addMasterString = "";
 
   $.ajax({
     url: "data/mh.json"
@@ -11,30 +11,64 @@ var masterString = "";
     // it reads the contents, and then executes whatever function
     // that you specify here in the done() method, and passes in
     // the contents of the file as the first argument.
-
-    // console.log("the contents of songs.json");
-    // console.log(contentsOfTheFile);
-    // console.log(contentsOfTheFile.songs)
-       for(var i= 0; i < contentsOfTheFile.songs.length; i++) {
-          var song = contentsOfTheFile.songs[i];
-        
-        var songElement = "<li><span class='song-name'>"+
-            song.title+" by "+song.artist+" on the album "+song.album+
-            "</span></li>"
-            console.log(songElement)             
-        masterString = masterString + songElement
+     for(var i= 0; i < contentsOfTheFile.songs.length; i++) {
+        var song = contentsOfTheFile.songs[i];
+      
+      var songElement = "<li><button class='delete'>Delete</button><span class='song-name'>"+
+          song.title+" by "+song.artist+" on the album "+song.album+
+          "</span></li>"
+          console.log(songElement)             
+      masterString = masterString + songElement
     }
-     
-     
-
-   
     // destination.html(masterString);
     console.log(masterString)
     // To completely replace the contents of a DOM element,
     // we used to write element.innerHTML = "something new";
-    $("#injectionSpot").html(masterString);
-
-       
-
+    $("#injectionSpot").html(masterString);     
   });
+
+//add more songs after splitting json file
+  function loadMore(){
+    $.ajax({
+      url: "data/mhaddl.json"
+    }).done(function(contentsOfTheFile) {
+    
+       for(var i= 0; i < contentsOfTheFile.songs.length; i++) {
+          var song = contentsOfTheFile.songs[i];
+        
+        var songElement = "<li><button class='delete'>Delete</button><span class='song-name'>"+
+            song.title+" by "+song.artist+" on the album "+song.album+
+            "</span></li>"
+            console.log(songElement)             
+        addMasterString = addMasterString + songElement
+      }
+      // destination.html(masterString);
+      console.log(addMasterString)
+      // To completely replace the contents of a DOM element,
+      // we used to write element.innerHTML = "something new";
+      $("#injectionSpot").append(addMasterString);     
+    });
+  }
+
+
+  $("#injectionSpot").on("click",".delete",deleteSong);
+
+  function deleteSong (event) {
+    var songToDelete = event.currentTarget.parentElement
+    $(songToDelete).remove();         
+    console.log(event.currentTarget)
+  }
+
+
+  $(".moreButton").click(loadMore);
+
+  
+
+
+
+//When the user clicks that button, load the songs from the second JSON file and append them to the bottom of the existing music, but before the More button.
+
+
+
 });
+
